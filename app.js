1da4d477b13847870d4f8199e171773e823c7f27
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-04-21 17:45:28 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-document.addEventListener('DOMContentLoaded',()=>{const btn=document.getElementById('runMinify');const input=document.getElementById('sourceCode');const output=document.getElementById('resultCode');btn.addEventListener('click',()=>{const raw=input.value;if(!raw)return alert('コードを入力してください');const minified=JSMinifier.minify(raw);output.value=minified;});});
+"use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('runMinify');
+    const input = document.getElementById('sourceCode');
+    const output = document.getElementById('resultCode');
+    if (!btn || !input || !output)
+        return;
+    btn.addEventListener('click', () => {
+        const raw = input.value;
+        if (!raw) {
+            alert('コードを入力してください');
+            return;
+        }
+        const minified = JSMinifier.minify(raw);
+        output.value = minified;
+    });
+});
